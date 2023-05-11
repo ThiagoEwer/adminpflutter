38edf0eadab6664981ac8136398745690b7c107b
ThiagoEwer <thiagoewer@gmail.com>
2023-05-11 11:55:36 -0300

    finalizado

---
+++ lib/website/crud/views/admin_menu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdminMenu extends StatefulWidget {
+  const AdminMenu({super.key});
+
+  @override
+  State<AdminMenu> createState() => _AdminMenuState();
+}
+
+class _AdminMenuState extends State<AdminMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
